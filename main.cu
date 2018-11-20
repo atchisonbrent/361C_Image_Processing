d00c7323b07f7b0ad6c85a7b044dd4b7de4e38dc
tachief <tachief@outlook.com>
2018-11-19 23:12:35 -0600

    testing bilateral

--- main.cu
+++ main.cu
@@ -137,37 +137,37 @@ const int sigma2 = 50;
 __device__ const int FILTER_SIZE = 9;
 __device__ const int FILTER_HALFSIZE = FILTER_SIZE >> 1;
 
-// __global__ 
-// void bilateral_filter_2d(unsigned char* input, unsigned char* output, int width, int height)
-// {
-// 	const int x = blockIdx.x * blockDim.x + threadIdx.x;
-// 	const int y = blockIdx.y * blockDim.y + threadIdx.y;
+__global__ 
+void bilateral_filter_2d(unsigned char* input, unsigned char* output, int width, int height)
+{
+	const int x = blockIdx.x * blockDim.x + threadIdx.x;
+	const int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-// 	if((x<width) && (y<height))
-// 	{
-// 		float running_total = 0;
-// 		float norm_factor = 0;
-// 		const int offset = y * width + x;
-// 		for (int xctr = -FILTER_HALFSIZE; xctr <= FILTER_HALFSIZE; xctr++) 
-// 		{
-// 			for (int yctr = -FILTER_HALFSIZE; yctr <= FILTER_HALFSIZE; yctr++) 
-// 			{
-// 				int y_iter = y + xctr;
-// 				int x_iter = x + yctr;
-// 				if (x_iter < 0) x_iter = -x_iter;
-// 				if (y_iter < 0) y_iter = -y_iter;
-// 				if (x_iter > width-1) x_iter = width-1-xctr;
-// 				if (y_iter > height-1) y_iter = height-1-yctr;
-// 				float intensity_change = input[y_iter * width + x_iter] - input[y * width + x];
-// 				float w1 = exp(-(xctr * xctr + yctr * yctr) / (2 * sigma1 * sigma1));
-// 				float w2 = exp(-(intensity_change * intensity_change) / (2 * sigma2 * sigma2));
-// 				running_total += input[y_iter * width + x_iter] * w1 * w2;
-// 				norm_factor += w1 * w2;
-// 			}
-// 		}
-// 		output[offset] = running_total / norm_factor;
-// 	}
-// }
+	if((x<width) && (y<height))
+	{
+		float running_total = 0;
+		float norm_factor = 0;
+		const int offset = y * width + x;
+		for (int xctr = -FILTER_HALFSIZE; xctr <= FILTER_HALFSIZE; xctr++) 
+		{
+			for (int yctr = -FILTER_HALFSIZE; yctr <= FILTER_HALFSIZE; yctr++) 
+			{
+				int y_iter = y + xctr;
+				int x_iter = x + yctr;
+				if (x_iter < 0) x_iter = -x_iter;
+				if (y_iter < 0) y_iter = -y_iter;
+				if (x_iter > width-1) x_iter = width-1-xctr;
+				if (y_iter > height-1) y_iter = height-1-yctr;
+				float intensity_change = input[y_iter * width + x_iter] - input[y * width + x];
+				float w1 = exp(-(xctr * xctr + yctr * yctr) / (2 * sigma1 * sigma1));
+				float w2 = exp(-(intensity_change * intensity_change) / (2 * sigma2 * sigma2));
+				running_total += input[y_iter * width + x_iter] * w1 * w2;
+				norm_factor += w1 * w2;
+			}
+		}
+		output[offset] = running_total / norm_factor;
+	}
+}
 
 void getError(cudaError_t err) {
     if(err != cudaSuccess) {
@@ -194,14 +194,14 @@ void filter (unsigned char* input_image, unsigned char* output_image, int width,
     // std::cout << "Blur Filter took " << (end-start)/CLOCKS_PER_SEC << " ms\n";
 
     /* Bilateral*/
-    const dim3 blockSize(32,32);
-    const dim3 gridSize(width/32, height/32);
+    const dim3 blockDims(64,64);
+    const dim3 gridDims(width/64, height/64);
     
     /* Invert */
     // invert<<<gridDims, blockDims>>>(dev_input, dev_output, width, height);
     
     /* Greyscale */
-    greyscale<<<gridDims, blockDims>>>(dev_input, dev_output, width, height);
+    bilateral_filter_2d<<<gridDims, blockDims>>>(dev_input, dev_output, width, height);
     
     getError(cudaMemcpy(output_image, dev_output, width*height*3*sizeof(unsigned char), cudaMemcpyDeviceToHost ));
 
